a5a64072d4ab3324d2d397bcbcc7a1e4e5be6e2c
Marjona <marjona@Marjonas-MacBook-Air.local>
2026-01-30 21:37:37 +0500

    Initial scikit-learn project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scikit-Learn Project
+
+This is a simple machine learning project using scikit-learn.
+
+## Features
+- Data loading
+- Model training
+- Evaluation
+
---
+++ main.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+
+X, y = load_iris(return_X_y=True)
+
+model = LogisticRegression(max_iter=200)
+model.fit(X, y)
+
+print("Accuracy:", model.score(X, y))
+
